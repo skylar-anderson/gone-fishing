b6749dd9af18c0b17513fa93a6041c159f68c6d4
Skylar Anderson <skylar-anderson@github.com>
2026-01-01 08:01:44 -0800

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: "/Users/skylar-anderson/code/fishing-game",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
